cd2ff993762e207dd72dc8d50f945a2ee37a9c62
Bayu Hafiz <23627311+bayoehafiz@users.noreply.github.com>
2024-06-15 13:58:59 +0700

    Add Laravel Health Library

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function (Request $request) {
+    return ['API Version' => app()->version()];
+});
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::get('/me', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::resource('products', ProductController::class);
 });
